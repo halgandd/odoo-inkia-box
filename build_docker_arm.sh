d7b7641d30dc273b955f3d67d051e5e5ea721ad9
Michel Perrocheau <mperrocheau@teclib.com>
2020-02-03 15:38:12 +0100

    [IMP] fix buildx arm

--- build_docker_arm.sh
+++ build_docker_arm.sh
@@ -38,8 +38,6 @@ if [ ${INSTALL}  -eq 1 ] ; then
   docker buildx rm armbuilder
   docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
   docker buildx create --name armbuilder --use
-
-
 fi
 
 
