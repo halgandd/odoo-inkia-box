562d7536583b431d47224dc255e5bf4abb7d8607
dhalgand <dhalgand@teclib.com>
2020-06-30 14:59:59 +0200

    [IMP] compose ports

--- build_and_push_arm_images.sh
+++ build_and_push_arm_images.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-
-docker buildx use armbuilder
-
-docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/teclib/odoo-box:printer_arm7 ./printer
-docker push registry.teclib-erp.com/teclib/odoo-box:printer_arm7
-docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/teclib/odoo-box:config_arm7 ./config
-docker push registry.teclib-erp.com/teclib/odoo-box:config_arm7
-docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/teclib/odoo-box:cups_arm7 ./cups
-docker push registry.teclib-erp.com/teclib/odoo-box:cups_arm7
+docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/docker/teclib-box:printer_arm7 ./printer
+docker push registry.teclib-erp.com/docker/teclib-box:printer_arm7
+docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/docker/teclib-box:config_arm7 ./config
+docker push registry.teclib-erp.com/docker/teclib-box:config_arm7
+docker buildx build --platform "linux/arm/v7" --load -t registry.teclib-erp.com/docker/teclib-box:cups_arm7 ./cups
+docker push registry.teclib-erp.com/docker/teclib-box:cups_arm7
