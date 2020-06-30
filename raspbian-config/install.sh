dd349445d93129d27b6aa475b694212e36e57a46
dhalgand <dhalgand@teclib.com>
2020-06-30 18:14:58 +0200

    [IMP] create image

---
+++ raspbian-config/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+########################################################################################################################
+# PARAMETERS
+########################################################################################################################
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied please add name for box"
+    exit 1
+fi
+
+NAME=$1
+USER=pi
+IP=192.168.0.31
+PASSWORD=raspberry
+NEW_PASSOWRD=teclib@
+PERSONNAL_TOKEN=HA3teNDxC6gtcsvVZ2UL
+PROJECT_ID=746
+
+########################################################################################################################
+# COPY SSH KEY CONFIG
+########################################################################################################################
+ssh-copy-id $USER@$IP
+
+########################################################################################################################
+# CREATE SSH KEY
+########################################################################################################################
+if ! [ -d "ssh" ]; then
+  mkdir ssh
+fi
+
+if ! [ -d "ssh/$NAME" ]; then
+  mkdir "ssh/$NAME"
+fi
+
+if ! [ -f "ssh/$NAME/id_rsa" ]; then
+  ssh-keygen -q -t rsa -N '' -C $NAME@teclib-box -f ssh/$NAME/id_rsa
+  GITLAB_DATA='{"title": "'$NAME@teclib-box'", "key": "'$(cat ssh/$NAME/id_rsa.pub)'", "can_push": "false"}'
+  curl --request POST --header "PRIVATE-TOKEN: $PERSONNAL_TOKEN" --header "Content-Type: application/json" --data "$GITLAB_DATA" "https://gitlab.teclib-erp.com/api/v4/projects/$PROJECT_ID/deploy_keys/"
+fi
+########################################################################################################################
+# GIT DEPLOY TOKEN
+########################################################################################################################
+if ! [ -f "ssh/$NAME/gitlab-token" ]; then
+  GITLAB_DATA='{"name": "'$NAME@teclib-box'", "expires_at": "2042-01-01", "username": "'$NAME'", "scopes": ["read_repository","read_registry","read_package_registry"]}'
+  curl --request POST --header "PRIVATE-TOKEN: $PERSONNAL_TOKEN" --header "Content-Type: application/json" --data "$GITLAB_DATA" "https://gitlab.teclib-erp.com/api/v4/projects/$PROJECT_ID/deploy_tokens/" | jq '.token' > ssh/$NAME/gitlab-token
+fi
+# TODO : fix authentification error with generated token and remove this line
+echo $PERSONNAL_TOKEN > ssh/$NAME/gitlab-david-token
+
+if ! [ -f "ssh/$NAME/gitlab-user" ]; then
+  echo $NAME@teclib-box > ssh/$NAME/gitlab-user
+fi
+
+########################################################################################################################
+# COPY FILE
+########################################################################################################################
+scp -rq ./datas/ $USER@$IP:/tmp/
+ssh $USER@$IP 'mkdir -p /tmp/ssh'
+scp -rq ./ssh/$NAME/* $USER@$IP:/tmp/ssh/
+
+########################################################################################################################
+# EXECUTE INSTALLATION
+########################################################################################################################
+ssh $USER@$IP 'bash -s' < ./raspbian.sh
---
+++ raspbian-config/raspbian.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+########################################################################################################################
+# SUDO
+########################################################################################################################
+sudo su
+
+########################################################################################################################
+# LOCALE
+########################################################################################################################
+echo "************************************************************************************"
+echo "Locale installation"
+echo "************************************************************************************"
+if [ "$(locale | grep LANG | cut -d= -f2)" != "fr_FR.UTF-8" ]; then
+  echo "Install locale fr_FR.UTF8"
+  raspi-config nonint do_change_locale fr_FR.UTF-8
+  raspi-config nonint do_configure_keyboard fr
+else
+  echo "Locale is already fr_FR.UTF8"
+fi
+
+########################################################################################################################
+# SSH
+########################################################################################################################
+mkdir -p /root/.ssh
+touch /root/.ssh/known_hosts
+mv /tmp/ssh/id_rsa /root/.ssh/
+mv /tmp/ssh/id_rsa.pub /root/.ssh/
+mv /tmp/ssh/gitlab-token /root/.ssh/
+mv /tmp/ssh/gitlab-david-token /root/.ssh/
+mv /tmp/ssh/gitlab-user /root/.ssh/
+chown root:root /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
+if [ -f "/tmp/ssh/teclib-box.env" ]; then
+  mv /tmp/ssh/teclib-box.env /root/.teclib-box.env
+fi
+rm -rf /tmp/ssh
+
+########################################################################################################################
+# PACKAGES
+########################################################################################################################
+echo "************************************************************************************"
+echo "Package installation"
+echo "************************************************************************************"
+apt update -y
+apt upgrade -y
+apt install -y figlet vim git
+
+
+
+# NGOK
+echo "************************************************************************************"
+echo "Ngok installation"
+echo "************************************************************************************"
+if ! [ -f "./ngrok.zip" ]; then
+  curl 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip' > ngrok.zip
+fi
+if ! [ -f "./ngrok" ]; then
+  unzip ngrok.zip
+fi
+
+# Docker
+echo "************************************************************************************"
+echo "Docker installation"
+echo "************************************************************************************"
+if ! [ -x "$(command -v docker)" ]; then
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+  usermod -aG docker pi
+else
+  echo "Docker already installed"
+fi
+
+# Docker compose
+echo "-----------------"
+echo "Docker Compose installation"
+echo "-----------------"
+if ! [ -x "$(command -v docker-compose)" ]; then
+  apt install -y python3-distutils python3-dev libffi-dev libssl-dev python3-pip
+  pip3 install docker-compose
+else
+  echo "Docker Compose already installed"
+fi
+
+########################################################################################################################
+# FILES
+########################################################################################################################
+echo "************************************************************************************"
+echo "Copy files"
+echo "************************************************************************************"
+cp -r /tmp/datas/* /
+rm -rf /tmp/datas
+########################################################################################################################
+# GIT
+########################################################################################################################
+echo "************************************************************************************"
+echo "Clone repository"
+echo "************************************************************************************"
+if [ ! -n "$(grep "^gitlab.teclib-erp.com" ~/.ssh/known_hosts)" ]; then
+  ssh-keyscan gitlab.teclib-erp.com >> ~/.ssh/known_hosts 2>/dev/null;
+  ssh-keyscan registry.teclib-erp.com >> ~/.ssh/known_hosts 2>/dev/null;
+fi
+
+if ! [ -d "/opt/teclib-box" ]; then
+  git clone git@gitlab.teclib-erp.com:docker/teclib-box.git /opt/teclib-box
+else
+  cd /opt/teclib-box
+  git pull
+  # TODO FIX
+#  echo $(cat /root/.ssh/gitlab-token) | docker login -u $(cat /root/.ssh/gitlab-user) --password-stdin registry.teclib-erp.com
+  echo $(cat /root/.ssh/gitlab-david-token) | docker login -u dhalgand@teclib.com --password-stdin registry.teclib-erp.com
+  ./start.sh
+fi
+
+########################################################################################################################
+# RESTART
+########################################################################################################################
+echo "************************************************************************************"
+echo "Reboot"
+echo "************************************************************************************"
+shutdown -r now
