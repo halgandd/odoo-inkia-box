3ba5f4cb9bad8482eae5ee21d7b54813baf8ed79
dhalgand <dhalgand@teclib.com>
2020-06-30 18:15:20 +0200

    [IMP]

--- create_image_rpi/raspberry_docker_download_images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget 'https://github.com/hypriot/image-builder-rpi/releases/download/v1.12.0/hypriotos-rpi-v1.12.0.img.zip' -O hypriotos.img.zip
-unzip hypriotos.img.zip
--- create_image_rpi/raspberry_docker_download_qemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget 'https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.14.79-stretch' -O kernel-qemu
-wget 'https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/versatile-pb.dtb'
-
